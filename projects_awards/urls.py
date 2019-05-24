19f352cde7ceced8976add958071b7e04dc4fc88
lizKimita <kimita.wanjiku@gmail.com>
2019-05-24 19:32:31 +0300

    making changes to the profile page and adding a footer

--- projects_awards/urls.py
+++ projects_awards/urls.py
@@ -10,6 +10,7 @@ urlpatterns=[
     url(r'^new_profile/$',views.new_profile,name = 'new_profile'),
     url(r'^edit_profile/$',views.edit_profile,name = 'edit_profile'),
 
+
 ]
 
 if settings.DEBUG:
