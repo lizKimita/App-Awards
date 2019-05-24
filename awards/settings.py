2c62c781b639a86e841fac31585d194c6303edf7
lizKimita <kimita.wanjiku@gmail.com>
2019-05-24 11:37:12 +0300

    adding the navbar and initial templates structure, views and url configurations

--- awards/settings.py
+++ awards/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'projects_awards', 
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
